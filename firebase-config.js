882bd55ef514b0215ca4064f8972d38ae31083ff
Ayan Charan Mummana <255977737+Quicxz@users.noreply.github.com>
2026-08-30 23:09:47 -0700

    Add config, cloud layer, and icon

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+/* Firebase web keys. App still works offline with PIN + local save. */
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBc07Zm_-NHP5vD42A1eiQbNaWJjTrKjMw",
+  authDomain: "calc-dojo.firebaseapp.com",
+  projectId: "calc-dojo",
+  storageBucket: "calc-dojo.firebasestorage.app",
+  messagingSenderId: "64609773442",
+  appId: "1:64609773442:web:4f47552a8572ab8b87919b"
+};
+window.DOJO_ALLOWED_EMAILS = [];
